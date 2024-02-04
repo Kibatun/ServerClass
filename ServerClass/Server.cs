ef96a71d2490ba5371f19b313c7aee48e241bf9e
Kibatun <ifkfvjif@mail.ru>
2024-02-05 02:51:26 +0500

    Добавьте файлы проекта.

---
+++ ServerClass/Server.cs
@@ -0,0 +1,34 @@
+﻿namespace ServerClass
+{
+    public class Server
+    {
+        private static readonly ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+        private static int count = 0;
+
+        public static int GetCount()
+        {
+            rwLock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                rwLock.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            rwLock.EnterWriteLock();
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                rwLock.ExitWriteLock();
+            }
+        }
+    }
+}
